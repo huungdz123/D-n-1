b03f351d721c7f817bc341b8b636d5a095008366
huungdz123 <huung2k.6@gmail.com>
2025-07-15 20:45:09 +0700

    Add files via upload
    up date 2 manhinh

---
+++ CategoryController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package baihoc.minimart.controller;
+
+import baihoc.minimart.entity.Category;
+
+/**
+ *
+ * @author NGUYEN HUU
+ */
+public interface CategoryController extends CrudController<Category>{
+    
+}
---
+++ UserController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package baihoc.minimart.controller;
+
+import baihoc.minimart.entity.User;
+
+/**
+ *
+ * @author NGUYEN HUU
+ */
+public interface UserController extends CrudController<User>{
+    
+}
